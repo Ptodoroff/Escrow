b5901fafe008579197ff2f232f21a4239b39a6bd
Petar <p.todorov212@gmail.com>
2022-08-18 19:18:21 +0300

    outlined the app.js skeleton

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,93 @@
-import React from "react";
-import "./App.css";
+import React, { Component } from 'react';
+import Escrow from '../../contracts/Escrow.json';
+import {getWeb3} from  './utils.js';
 
-function App() {
-  return (
-   <div classNmae = "container">
-     <h1 className="text-center">Escrow</h1>
+class App extends Component {
+  
+  state = {
+    web3: undefined,
+    accounts:[],
+    currentAccount:undefined,
+    contract: undefined,
+    balance: undefined
 
-     <div className = "row">
-      <div className ="col-sm-12">
-         <p> Balance: <b></b>wei</p>
+  } 
+  
+  async componentDidMount ()  {
+    const web3 = await getWeb3();
+    const accounts = web3.eth.getAccounts();
+    const networkId = await web3.eth.net.getId();
+    const contract = new web3.eth.Contract(
+      Escrow.abi,
+      deployedNetwork && deployedNetwork.address,
+    );
+    this.setState({ web3, accounts, contract }, this.updateBalance);
+  }
+
+  async updateBalance() {
+      const { contract } = this.state;
+      const balance = await contract.methods.balanceOf().call();
+      this.setState({ balance });
+    };
+
+    async deposit(e) {
+      e.preventDefault();
+      const { contract, accounts } = this.state;
+      await contract.methods.deposit().send({
+        from: accounts[0], 
+        value: e.target.elements[0].value
+      });
+      this.updateBalance();
+    }
+
+  
+
+
+
+    
+
+render() {
+  if (!this.state.web3) {
+    return <div>Loading...</div>;
+  }
+
+  const { balance } = this.state;
+
+
+
+    return (
+      <div className="container">
+        <h1 className="text-center">Escrow</h1>
+
+        <div className="row">
+          <div className="col-sm-12">
+             <p>Balance: <b> {balance}</b> wei </p>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-sm-12">
+            <form onSubmit={e => this.deposit(e)}>
+              <div className="form-group">
+                <label htmlFor="deposit">Deposit</label>
+                <input type="number" className="form-control" id="deposit" />
+              </div>
+              <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
+          </div>
+        </div>
+
+        <br />
+
+        <div className="row">
+          <div className="col-sm-12">
+             <button onClick = {() => this.release()} type="submit" className="btn btn-primary">Release</button>
+          </div>
+        </div>
+
       </div>
-     </div>
-
-     <div className = "row">
-       <div className="col-sm-12"
-     </div>
-
-
-
-
-
-
-
-   </div>
-
-
-
-  );
+    );
+  }
 }
 
 export default App;
