210e4b3e472117d6e1ec1cba9f93045ecb6d000d
Petar <p.todorov212@gmail.com>
2022-08-18 18:35:29 +0300

    added react project environment and started writing the  App.js file

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+   <div classNmae = "container">
+     <h1 className="text-center">Escrow</h1>
+
+     <div className = "row">
+      <div className ="col-sm-12">
+         <p> Balance: <b></b>wei</p>
+      </div>
+     </div>
+
+     <div className = "row">
+       <div className="col-sm-12"
+     </div>
+
+
+
+
+
+
+
+   </div>
+
+
+
+  );
+}
+
+export default App;
